c44d183be46fdabf76be72f5d45493807522a699
pramodJ <pramodjm4@gmail.com>
2023-04-22 12:05:32 +0530

    making validation

--- Model/EmpSchema.js
+++ Model/EmpSchema.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose")
 
 let EmpModel=mongoose.Schema({
-    name:{type:String},
-    email:{type:String},
-    password:{type:String},
+    name:{type:String,require:true},
+    email:{unique:true , type: String, required: true },
+    password:{ type: String, required: true },
 })
 
 module.exports=mongoose.model("EmpModel",EmpModel)
